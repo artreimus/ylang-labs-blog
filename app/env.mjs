7c0214a654029bbfce140be5c3ef2cbe56b68c97
Arthur Reimus <arthurreimus1623@gmail.com>
2025-03-15 17:42:22 +0800

    refactor: Update environment configuration by removing unused variables

--- app/env.mjs
+++ app/env.mjs
@@ -3,16 +3,12 @@ import { z } from 'zod'
 
 export const env = createEnv({
   client: {
-    APP_ENV: z.enum(['local', 'dev', 'production']).optional(),
-    NEXT_PUBLIC_APP_URL: z.string().min(1),
     NEXT_PUBLIC_YLANG_API_URL: z.string().min(1),
     NEXT_PUBLIC_YLANG_API_KEY: z.string().optional(),
     NEXT_PUBLIC_YLANG_API_AUTH: z.string().optional(),
   },
   server: {},
   runtimeEnv: {
-    APP_ENV: process.env.APP_ENV,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_YLANG_API_URL: process.env.NEXT_PUBLIC_YLANG_API_URL,
     NEXT_PUBLIC_YLANG_API_KEY: process.env.NEXT_PUBLIC_YLANG_API_KEY,
     NEXT_PUBLIC_YLANG_API_AUTH: process.env.NEXT_PUBLIC_YLANG_API_AUTH,
