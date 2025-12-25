8e26d5fec31cfd06cec62bf793b5f0ccecf46918
artreimus <artrei.dev@gmail.com>
2025-12-25 16:46:11 +0800

    style: Adjust horizontal padding for navigation components

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -108,7 +108,7 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
         minWidth: '800px',
       }}
       className={cn(
-        'relative z-70 mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-4 py-1.5 dark:bg-transparent lg:flex',
+        'relative z-70 mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-8 py-1.5 dark:bg-transparent lg:flex',
         visible && 'bg-white/80 dark:bg-neutral-950/80',
         className
       )}
@@ -159,8 +159,8 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
           ? '0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset'
           : 'none',
         width: visible ? '90%' : '100%',
-        paddingRight: visible ? '12px' : '0px',
-        paddingLeft: visible ? '12px' : '0px',
+        paddingRight: visible ? '32px' : '16px',
+        paddingLeft: visible ? '32px' : '16px',
         borderRadius: '9999px',
         y: visible ? 20 : 0,
       }}
